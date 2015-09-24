50e3d26bc722b2bd7a779d7c1f6ec10a9def8b7c
synapticnet <richardtodd@synapticnet.com>
2015-09-24 16:33:15 -0500

    First commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates an object that holds a matrix and its inverse.
+## cacheSolve returns the inverse of the matrix stored in a
+## makecacheMatrix object.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+## creates an object that holds a matrix and can store its inverse.
+## get() retruns the original matrix. getInv() returns the inverse matrix
+## if it has been calculated, if not returns null.  Set() stores a matrix and
+## removes any stored inverse matrix.
+makeCacheMatrix <- function(mtx = matrix()) {
+    
+    invMtx <- NULL
+    set <- function(y){
+        mtx <<- y
+        invMtx <<- NULL
+    }
+    get <- function() mtx
+    
+    setInv <- function(inv) invMtx <<- inv
+    getInv <- function() invMtx
+    
+    isCachedMatrix <- TRUE
+    
+    list(set = set, get = get, setInv = setInv, 
+         getInv = getInv, isCachedMatrix = isCachedMatrix)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Input: Takes a makeCachMatrix object
+## output: returns the inverse of the matrix stored in the makeCachMatrix object.
+##      If that object does not contain the inverse matrix it will calculate
+##      the inverse, store it in the makeCachMatrix object, then return the result.
+cacheSolve <- function(cachedMatrix, ...) {
+    
+    inv <- cachedMatrix$getInv()
+    if(!is.null(inv)){
+        return(inv)
+    }
+    
+    mtx <- cachedMatrix$get()
+    inv <- solve(mtx, ...)
+    cachedMatrix$setInv(inv)
+    inv
 }
